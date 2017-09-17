a8df5b5a6b7a411960a29526bf3276b23cb7a904
TarCode <tarcode33@gmail.com>
2017-09-17 23:35:40 +0200

    connected redux store to app via provider

--- app/index.js
+++ app/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     AppRegistry,
     Text,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux'
-
+import store from './store'
 
 import HomeScreen from './components/home'
 import TodoScreen from './components/todo'
 
-export default StackNavigator({
+const App = StackNavigator({
     Home: { screen: HomeScreen },
     Todo: { screen: TodoScreen }
 });
 
-// export default <Provider store={store}>
+export default class extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-// </Provider>
+    render() {
+        return (
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        )
+    }
+}
