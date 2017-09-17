035d83dc2850bebb8ba73be03b3ccf1159d3ccef
TarCode <tarcode33@gmail.com>
2017-09-17 23:39:35 +0200

    connected actions and state to todo component

--- app/components/todo.js
+++ app/components/todo.js
@@ -9,7 +9,7 @@ import { RkButton, RkTextInput } from 'react-native-ui-kitten';
 
 
 
-export default class supertodo extends Component {
+class supertodo extends Component {
     static navigationOptions = {
         title: 'SuperTodo',
         headerTintColor: '#FFF',
@@ -28,6 +28,39 @@ export default class supertodo extends Component {
         );
     }
 }
+
+import { connect } from 'react-redux'
+import { toggleTodo } from '../actions'
+
+const getVisibleTodos = (todos, filter) => {
+    switch (filter) {
+        case 'SHOW_ALL':
+            return todos
+        case 'SHOW_COMPLETED':
+            return todos.filter(t => t.completed)
+        case 'SHOW_ACTIVE':
+            return todos.filter(t => !t.completed)
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTodoClick: id => {
+            dispatch(toggleTodo(id))
+        }
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(supertodo)
 
 const styles = StyleSheet.create({
     container: {
